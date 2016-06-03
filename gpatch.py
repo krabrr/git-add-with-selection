44c204bdfe6371fd15fed1a07005ea7496c15dd8
Nueng <nueng@thinkcubic.com>
2016-06-03 15:25:59 +0700

    update gpatch param

--- gpatch.py
+++ gpatch.py
@@ -3,7 +3,8 @@ import os
 import sys
 import subprocess
 
-if len(sys.argv) < 2:
+filename = 'fix.patch'
+if len(sys.argv) <= 2:
     p = subprocess.Popen(['git', 'log', '-1'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     output, err = p.communicate(b"input data that is passed to subprocess' stdin")
     output = output.split('\n')[0]
@@ -14,12 +15,13 @@ if len(sys.argv) < 2:
     else:
         print 'Error: cannot get commit version'
         sys.exit()
-elif len(sys.argv) == 2:
-    commit_version = sys.argv[1]
-    prev_version = commit_version+'~1'
-elif len(sys.argv) == 3:
+    if len(sys.argv) == 2:
+        filename = sys.argv[1]
+elif len(sys.argv) <= 4:
     prev_version = sys.argv[1]
     commit_version = sys.argv[2]
+    if len(sys.argv) == 4:
+        filename = sys.argv[3]
 
 print 'prev version: '+prev_version
 print 'current version: '+commit_version
@@ -27,7 +29,7 @@ print 'current version: '+commit_version
 p = subprocess.Popen(['git', 'diff', prev_version, commit_version], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 output, err = p.communicate(b"input data that is passed to subprocess' stdin")
 print output
-out = open(os.path.expanduser('fix.patch'), 'w')
+out = open(os.path.expanduser('~/' + filename), 'w')
 out.write(output)
 out.close()
 
